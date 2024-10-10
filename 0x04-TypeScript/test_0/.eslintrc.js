c6bc63405a42c0a036235b719e384b9440aa141a
soufianetangi <soufianetangi2@gmail.com>
2024-10-10 00:12:56 +0000

    Updated

---
+++ 0x04-TypeScript/test_0/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+  ],
+  parserOptions:  {
+    ecmaVersion:  2024,
+    sourceType:  'module',
+  },
+  rules:  {
+  },
+};
